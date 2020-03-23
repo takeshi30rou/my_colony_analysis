93a036c93610fe3eaa611e3a38967b12f88b1b07
mkdy39 <52729848+mkdy39@users.noreply.github.com>
2020-03-23 14:04:21 +0900

    Update test_get_table.py

--- test_get_table.py
+++ test_get_table.py
@@ -7,9 +7,6 @@ import pickle
 if __name__ == '__main__':
   config = configparser.ConfigParser()
   config.read('pict2colony.ini')
-  p2c.get_colony_table(config)
-  # with open('colony_table.pkl', 'rb') as f:
-  #   colony_table = pickle.load(f)
-  # growth_talbe = c2g.get_growth_table(colony_table)
-  # ngrowth_table = g2n.get_ngrowth_table(growth_talbe)
-  # print(ngrowth_table)
+  colony_table = p2c.get_colony_table(config)
+  growth_talbe = c2g.get_growth_table(colony_table)
+  ngrowth_table = g2n.get_ngrowth_table(growth_talbe)
