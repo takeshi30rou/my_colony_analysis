2181fc53b949003cd52a56703eba1149ac10c7c7
takeshi30rou <takeshi30rou@gmail.com>
2020-07-04 01:06:31 +0900

    Update test_get_table.py
    不要なimportの削除

--- colony_analysis/test_get_table.py
+++ colony_analysis/test_get_table.py
@@ -2,7 +2,6 @@ import colony_analysis.pict2colony as p2c
 import colony_analysis.colony2growth as c2g
 import colony_analysis.growth2ngrowth as g2n
 import configparser
-import pickle
 
 if __name__ == '__main__':
     config = configparser.ConfigParser()
