0d4507e55e0cd983f1e9392880be17db6cf84116
takeshi <takeshi30rou@gmail.com>
2020-07-07 21:32:01 +0900

    add test

--- tests/test_growth2ngrowth.py
+++ tests/test_growth2ngrowth.py
@@ -1,6 +1,7 @@
 import colony_analysis.growth2ngrowth as g2n
 import pandas as pd
 import numpy as np
+import csv
 
 growth_talbe = pd.read_csv("tests/hoge2.csv").values.tolist()
 growth_talbe = [['Column', 'Row', 'CONV', 'LTG', 'MGR', 'SPG']] + growth_talbe
@@ -25,12 +26,36 @@ def _load_table(table):
             ary[row - 1, col - 1, ind] = v
     return ary, poss
 
+def _load_csv(fname):
+    header = 1
+    poss = []
+    ary = np.zeros((32, 48, 4))
+    for items in csv.reader(open(fname, 'r')):
+        if header:
+            header -= 1
+            continue
+        col = int(items[0])
+        row = int(items[1])
+        poss += [[col, row]]
+        v = float(items[2])
+        for ind in range(4):
+            v = float(items[2 + ind])
+            ary[row - 1, col - 1, ind] = v
+    return ary, poss
+
 def test_get_growth_table():
     assert ngrowth_table == g2n.get_ngrowth_table(growth_talbe)
 
 def test_load_table():
     expected = _load_table(growth_talbe)
     result = g2n.load_table(growth_talbe)
+
+    np.testing.assert_allclose(expected[0], result[0]) # ary
+    assert expected[1] == result[1] # poss
+
+def test_load_csv():
+    expected = _load_csv("tests/hoge2.csv")
+    result = g2n.load_csv("tests/hoge2.csv")
 
     np.testing.assert_allclose(expected[0], result[0]) # ary
     assert expected[1] == result[1] # poss
